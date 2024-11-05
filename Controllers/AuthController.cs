317c8a6a0740f5bf3e82297469272ec53bb91b19
Dang Huy <nhathuydangnguyen@gmail.com>
2024-11-06 01:54:52 +0700

    Merge changes folder Controllers from master

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -19,13 +19,23 @@ namespace RentCarSystem.Controllers
         private readonly RentCarSystemContext dbContext;
         private readonly IMapper mapper;
         private readonly IRegisterReponsitory registerReponsitory;
+<<<<<<< HEAD
+        private readonly ILoginReponsitory loginReponsitory;
+
+        public AuthController(ITokenReponsitory tokenReponsitory, RentCarSystemContext dbContext, IMapper mapper, IRegisterReponsitory registerReponsitory, ILoginReponsitory loginReponsitory)
+=======
 
         public AuthController(ITokenReponsitory tokenReponsitory, RentCarSystemContext dbContext, IMapper mapper, IRegisterReponsitory registerReponsitory)
+>>>>>>> 245adf983c80f561f0c244ccf5e507c9b3b495e7
         {
             this.tokenReponsitory = tokenReponsitory;
             this.dbContext = dbContext;
             this.mapper = mapper;
             this.registerReponsitory = registerReponsitory;
+<<<<<<< HEAD
+            this.loginReponsitory = loginReponsitory;
+=======
+>>>>>>> 245adf983c80f561f0c244ccf5e507c9b3b495e7
         }
     
 
@@ -41,7 +51,11 @@ namespace RentCarSystem.Controllers
 
                 //Map DTO  to Domain User
                 var userDomain = mapper.Map<User>(registerRequestDTO);
+<<<<<<< HEAD
+                await registerReponsitory.RegisterUser(userDomain,registerRequestDTO.Password, registerRequestDTO.Roles);
+=======
                 await registerReponsitory.RegisterUser(userDomain,registerRequestDTO.Password);
+>>>>>>> 245adf983c80f561f0c244ccf5e507c9b3b495e7
 
                 // Map DTO to Role and get UserId
                 var roleDomain = mapper.Map<Role>(registerRequestDTO);
@@ -62,6 +76,43 @@ namespace RentCarSystem.Controllers
         }
 
 
+<<<<<<< HEAD
+        //POST: /api/Auth/Login
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequestDTO)
+        {
+            //Retrive user 
+            var user = await dbContext.Users
+                .Include(u => u.Roles)
+                .FirstOrDefaultAsync(u => u.Email == loginRequestDTO.email);
+
+            if (user != null)
+            {
+                //Verify password
+                var isPasswordValid = await loginReponsitory.VerifyPassword(user, loginRequestDTO.password);
+
+                if(isPasswordValid)
+                {
+                    //Get Role for this User
+                    var role = user.Roles.FirstOrDefault()?.Type;
+                    if(role != null)
+                    {
+                        //Create Token 
+                        var jwtToken = tokenReponsitory.CreateJWTToken(user, new List<string> { role });
+                        return Ok(new {Token = jwtToken});
+                    }
+                }
+                return Unauthorized("Incorrect password ...");
+            }
+            
+            return Unauthorized("User not existing ...");
+
+        }
+
+
+=======
+>>>>>>> 245adf983c80f561f0c244ccf5e507c9b3b495e7
 
         private async Task<IActionResult> RegisterAdmin(User userDomain, Role roleDomain)
         {
@@ -178,7 +229,11 @@ namespace RentCarSystem.Controllers
                 var approvalRequest = new ApprovalRequest
                 {
                     //admin id
+<<<<<<< HEAD
+                    AdminId = admId, //convert string to Guid Guid.Parse(admId),
+=======
                     AdminId = Guid.Parse(admId), //convert string to Guid
+>>>>>>> 245adf983c80f561f0c244ccf5e507c9b3b495e7
                     BsnId = businessDomain.BsnId,
                     RequestDay = DateOnly.FromDateTime(DateTime.Now),
                     Status = "PENDING"
@@ -190,7 +245,11 @@ namespace RentCarSystem.Controllers
                 var notificationDefault = new Notification
                 {
                     SenderId = userDomain.UserId,
+<<<<<<< HEAD
+                    ReceiverId = admId,//Guid.Parse(admId),
+=======
                     ReceiverId = Guid.Parse(admId),
+>>>>>>> 245adf983c80f561f0c244ccf5e507c9b3b495e7
                     Message = "New Business Registration requires approval.",
                     NotificationDate = DateOnly.FromDateTime(DateTime.Now)
                 };
